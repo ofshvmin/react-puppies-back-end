1574688d9113480302181363dc721fb26d134eab
Daniel Mathews <daniel.g.mathews@gmail.com>
2024-04-26 20:01:54 -0400

    implement full crud on puppy resource

--- controllers/puppies.js
+++ controllers/puppies.js
@@ -10,8 +10,50 @@ async function create(req, res) {
   }
 }
 
+async function index(req, res) {
+  try {
+    const puppies = await Puppy.find({}).sort({createdAt: 'desc'})
+    res.json(puppies)
+  } catch (err) {
+    console.log(err)
+    res.json(err)
+  }
+}
 
+async function deletePuppy(req, res) {
+  try {
+    const puppy = await Puppy.findByIdAndDelete(req.params.puppyId)
+    res.json(puppy)
+  } catch (err) {
+    console.log(err)
+    res.json(err)
+  }
+}
+async function update(req, res) {
+  try {
+    const puppy = await Puppy.findByIdAndUpdate(req.params.puppyId, req.body, {new: true})
+    res.json(puppy)
+  } catch (err) {
+    console.log(err)
+    res.json(err)
+  }
+}
+
+
+async function show(req, res) {
+  try {
+    const puppy = await Puppy.findById(req.params.puppyId)
+    res.json(puppy)
+  } catch (err) {
+    console.log(err)
+    res.json(err)
+  }
+}
 
 export {
   create,
+  index,
+  deletePuppy as delete,
+  show,
+  update,
 }
--- routes/puppies.js
+++ routes/puppies.js
@@ -3,7 +3,12 @@ import * as puppiesCtrl from '../controllers/puppies.js'
 
 const router = Router()
 
+
+router.get('/', puppiesCtrl.index)
+router.get('/:puppyId', puppiesCtrl.show)
 router.post('/', puppiesCtrl.create)
+router.put('/:puppyId', puppiesCtrl.update)
+router.delete('/:puppyId', puppiesCtrl.delete)
 
 export {
   router
